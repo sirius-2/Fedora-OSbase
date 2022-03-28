c804ac0baca42f38fb290177f642497698a8aeb6
Сириус2 <xjy37.me@outlook.com>
2022-03-28 03:30:07 +0000

    update setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,43 +1,49 @@
 #!/bin/bash
 
 ## Variables
-result_dir='./Result'
-tmp_dir='./tmp'
+cDir=`pwd`
+tmp_dir="$cDir/tmp"
 dsdt_ori='/sys/firmware/acpi/tables/DSDT'
 
-## chmod && makedir
-mkdir $result_dir $tmp_dir
-sudo chmod -R 777 .
+## makedir
+if [[ ! -d $tmp_dir ]];then mkdir -p $tmp_dir;else sudo rm -rf $tmp_dir && mkdir $tmp_dir;fi
+rm -f *.zip
 
 ## Get DSDT.dsl
 if [[ -f $dsdt_ori ]];then
-	sudo cat $dsdt_ori > $result_dir/DSDT.dat
+	sudo cp -R /sys/firmware/acpi/tables $tmp_dir
 	cp -f iasl-prebuilt/linux/iasl iasl && sudo chmod +x ./iasl
-	./iasl -d $result_dir/DSDT.dat
 else
 	echo '[ x ] No DSDT found'
 fi
 
 ## lspci
-lspci > $result_dir/lspci.txt
+lspci > $tmp_dir/lspci.txt
 
 ## Get Codec#0 for ApppleALC
-cp /proc/asound/card0/codec* $tmp_dir
-sudo cp -R /sys/firmware/acpi/tables $tmp_dir
-if [[ -f '$tmp_dir/codec#0' ]];then
-	sudo chmod a+x verbit.sh
-	./verbit.sh '$tmp_dir/codec#0' > $tmp_dir/ALC_dump.txt
+sudo cp /proc/asound/card0/codec* $tmp_dir
+cp verbit.sh $tmp_dir
+sudo chmod a+x $tmp_dir/verbit.sh
+
+### Handle
+sudo chmod -R 777 $tmp_dir
+./iasl -d $tmp_dir/tables/DSDT
+cd $tmp_dir
+if [[ -f 'codec#0' ]];then
+	./verbit.sh 'codec#0' > ALC_dump.txt
 else
 	echo '[ x ] No codec#0 found'
 fi
 
 ## cp files & zip
-cp -f 'codec#0' $result_dir
-cp -f 'ALC_dump.txt' $result_dir
+cp -f $tmp_dir/tables/DSDT.dsl $tmp_dir
+zip -q -r ../OpenCoreBase.zip ./* -x=./verbit.sh
+#sudo rm -rf $tmp_dir
 cd ..
-zip -q -r $result_dir OpenCoreBase.zip
+### Handle End
 
 ## More FakeEC
+#clear
 cat<<EOF
 [ - ] More eg.: FakeEC
                   https://github.com/corpnewt/SSDTTime
